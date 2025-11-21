6c9c84132c486a10efae9246fce1ac0113b3bede
Aravindto <34885976+Aravindto@users.noreply.github.com>
2025-11-21 23:27:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import usersRoutes from "./routes/users.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json()); // To parse JSON body
+
+// Routes
+app.use("/users", usersRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
